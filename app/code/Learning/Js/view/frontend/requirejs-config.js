d04bd4c56ec5ad4197e9abaa389233192ee604a3
LI bo <nomius1990@gmail.com>
2018-09-13 11:53:31 +0800

    自己研究js结果
    最重要的是requirejs-config.js中的的内容:
    前面的bbbb 对应的是hello.html 里面的 键值,后面代表的事js目录里面的js文件

--- app/code/Learning/Js/view/frontend/requirejs-config.js
+++ app/code/Learning/Js/view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     map: {
         '*': {
-            hello:           'Learning_Js/js/hello',
+            bbbb:           'Learning_Js/js/bbbb', // 前面的bbbb 对应的是hello.html 里面的 键值,后面代表的事js目录里面的js文件
+            cccc:           'Learning_Js/js/cccc', // 和上面一样啦
         }
     }
 };
--- app/code/Learning/Js/view/frontend/web/js/bbbb.js
+++ app/code/Learning/Js/view/frontend/web/js/bbbb.js
@@ -3,6 +3,7 @@ define([
     ], function($){
         "use strict";
         return function(config, element) {
+            console.log(config);
             alert(config.message);
         }
     }
