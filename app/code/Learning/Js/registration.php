ede9e41a183221660c2142675b2d69cba73444c1
duty.li <duty.li@silksoftware.com>
2018-09-26 10:04:35 +0800

    接下来开始自己开发主题,先提交一把
    自己开发主题先提交一把

--- app/code/Learning/Js/registration.php
+++ app/code/Learning/Js/registration.php
@@ -6,6 +6,8 @@
  * Time: 10:06
  */
 
-\Magento\Framework\Component\ComponentRegistrar::register( \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Learning_Js',
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Learning_Js',
     __DIR__
 );
